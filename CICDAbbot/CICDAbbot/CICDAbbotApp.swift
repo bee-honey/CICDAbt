34eaa177e7b5e894eca140a77bb5ce72dec265b1
bee-honey <honeybeeprogrammer@gmail.com>
2024-10-25 14:27:45 -0700

    Basic initialization of project

---
+++ CICDAbbot/CICDAbbot/CICDAbbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDAbbotApp.swift
+//  CICDAbbot
+//
+//  Created by Naveen Keerthy on 10/25/24.
+//
+
+import SwiftUI
+
+@main
+struct CICDAbbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
